f278ce05aca09d98772f5125960737208f1bd8b3
jeremymarch <jmarch@gradcenter.cuny.edu>
2022-03-29 10:48:58 -0400

    fix bug in last commit: midi needs to be outside of the closure and _conn_in needs to be scoped outside of the if so that it will not be dropped

--- src/main.rs
+++ src/main.rs
@@ -711,51 +711,53 @@ fn main() {
             .content(&vbox)
             .build();
         window.show();
-
-        let mut input = String::new();
-    
-        let mut midi_in = MidiInput::new("midir reading input").unwrap();
-        midi_in.ignore(Ignore::None);
-        
-        // Get an input port (read from console if multiple are available)
-        let in_ports = midi_in.ports();
-        if in_ports.len() > 0 {
-            let in_port = &in_ports[0];
-            /* 
-            let in_port = match in_ports.len() {
-                0 => panic!("no ports"),
-                _ => {
-                    println!("Choosing the only available input port: {}", midi_in.port_name(&in_ports[0]).unwrap());
-                    &in_ports[0]
-                },
-                
-                _ => {
-                    println!("\nAvailable input ports:");
-                    for (i, p) in in_ports.iter().enumerate() {
-                        println!("{}: {}", i, midi_in.port_name(p).unwrap());
-                    }
-                    print!("Please select input port: ");
-    
-                    stdout().flush()?;
-                    let mut input = String::new();
-                    stdin().read_line(&mut input)?;
-                    in_ports.get(input.trim().parse::<usize>()?)
-                            .ok_or("invalid input port selected")?
-                }
-            };
-            */
-            //println!("\nOpening connection");
-            //let in_port_name = midi_in.port_name(in_port).unwrap();
-            
-            // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
-            let log_all_bytes:Vec<Vec<u8>> = Vec::new();
-            let _conn_in = midi_in.connect(in_port, "midir-read-input", move |stamp, message, log| {
-                println!("{}: {:?} (len = {})", stamp, message, message.len());
-                log.push(message.to_vec());
-            }, log_all_bytes).unwrap();
-        }
     });
     
+    let mut input = String::new();
+    
+    let mut midi_in = MidiInput::new("midir reading input").unwrap();
+    midi_in.ignore(Ignore::None);
+    
+    // Get an input port (read from console if multiple are available)
+    let in_ports = midi_in.ports();
+    let mut _conn_in;
+    if in_ports.len() > 0 {
+        let in_port = &in_ports[0];
+        /* 
+        let in_port = match in_ports.len() {
+            0 => panic!("no ports"),
+            _ => {
+                println!("Choosing the only available input port: {}", midi_in.port_name(&in_ports[0]).unwrap());
+                &in_ports[0]
+            },
+            
+            _ => {
+                println!("\nAvailable input ports:");
+                for (i, p) in in_ports.iter().enumerate() {
+                    println!("{}: {}", i, midi_in.port_name(p).unwrap());
+                }
+                print!("Please select input port: ");
+
+                stdout().flush()?;
+                let mut input = String::new();
+                stdin().read_line(&mut input)?;
+                in_ports.get(input.trim().parse::<usize>()?)
+                        .ok_or("invalid input port selected")?
+            }
+        };
+        */
+        //println!("\nOpening connection");
+        //let in_port_name = midi_in.port_name(in_port).unwrap();
+        
+        // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
+        let log_all_bytes:Vec<Vec<u8>> = Vec::new();
+        _conn_in = midi_in.connect(in_port, "midir-read-input", move |stamp, message, log| {
+            println!("{}: {:?} (len = {})", stamp, message, message.len());
+            log.push(message.to_vec());
+        }, log_all_bytes).unwrap();
+    }
+
+
     /* 
     let source;
     let input_port;
